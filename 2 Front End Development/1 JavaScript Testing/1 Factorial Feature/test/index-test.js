f2df3b04bfa0876741d09b66d7ad76dec7be9783
londonrsteele <londonrsteele@gmail.com>
2024-09-17 15:56:10 -0500

    Refactor - Part 1

--- 2 Front End Development/1 JavaScript Testing/1 Factorial Feature/test/index-test.js
+++ 2 Front End Development/1 JavaScript Testing/1 Factorial Feature/test/index-test.js
@@ -4,7 +4,12 @@ var Calculate =  require('../index.js')
 describe('Calculate', () => {
   describe('.factorial', () => {
     it('test if 5! is equal to 120', () => {
-        assert.equal(Calculate.factorial(5), 120);
+        // Setup
+        const expected = 120;
+        // Exercise
+        const actual = Calculate.factorial(5);
+        // Verify
+        assert.equal(actual, expected);
     });
   });
 });
