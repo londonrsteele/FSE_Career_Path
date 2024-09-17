41d76c46dd3efb0012d038421cc85604f4be99b0
londonrsteele <londonrsteele@gmail.com>
2024-09-17 16:01:21 -0500

    Get into the Green - Part 3 (edge case)

--- 2 Front End Development/1 JavaScript Testing/1 Factorial Feature/index.js
+++ 2 Front End Development/1 JavaScript Testing/1 Factorial Feature/index.js
@@ -1,5 +1,8 @@
 const Calculate = {
     factorial(num) {
+        if( num === 0 ) {
+            return 1;
+        }
         for (let i = num - 1; i >= 1; i--) {
             num = num * i; 
         }
