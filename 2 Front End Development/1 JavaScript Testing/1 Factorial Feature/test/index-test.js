4401feab7f8ad2645a5409db5a6475b54efc9bec
londonrsteele <londonrsteele@gmail.com>
2024-09-17 15:52:38 -0500

    Get into the Red - Part 1

--- 2 Front End Development/1 JavaScript Testing/1 Factorial Feature/test/index-test.js
+++ 2 Front End Development/1 JavaScript Testing/1 Factorial Feature/test/index-test.js
@@ -3,6 +3,8 @@ var Calculate =  require('../index.js')
 
 describe('Calculate', () => {
   describe('.factorial', () => {
-
+    it('test if 5! is equal to 120', () => {
+        assert.equal(Calculate.factorial(5), 120);
+    });
   });
 });
